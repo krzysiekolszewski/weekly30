e7dec0b7df4d364d17f924f1da4669e6911be656
krzysiekolszewski <krzols95@gmail.com>
2017-07-02 09:49:53 +0200

    responsive menu, smooth scrolling effect

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 require('../sass/main.scss');
+
+$(document).ready(function() {
+
+    //scroll down animation
+    $('ul a').click(function() {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 1000);
+        return false;
+    });
+
+    //mobile nav
+    $(".fa-bars").on('click', function() {
+        $('.overlay').css('display', 'flex');
+    })
+
+    $(".fa-times").on('click', function() {
+        $('.overlay').css('display', 'none');
+    })
+
+    $(".overlay a").on('click', function() {
+        $('.overlay').css('display', 'none');
+    })
+
+});
